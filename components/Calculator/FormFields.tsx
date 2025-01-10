c97586251ea16c275730f4815050d883ff5c33c9
RubenHaisma <74268046+RubenHaisma@users.noreply.github.com>
2025-01-10 16:56:38 +0100

    add casing

--- components/Calculator/FormFields.tsx
+++ components/Calculator/FormFields.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { UseFormRegister, FieldErrors } from 'react-hook-form';
 import { StudentInfo } from '../../types/calculator';
-import Tooltip from '../ui/Tooltip';
+import Tooltip from '../UI/Tooltip';
 
 interface FormFieldsProps {
   register: UseFormRegister<StudentInfo>;
